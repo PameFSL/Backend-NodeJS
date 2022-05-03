de9e165363a36d725029e23d527cc52286e1a247
Karma <btobteen@yahoo.es>
2022-05-03 16:41:47 -0500

    Semana 1: Ejercicio 2

--- playbook-main/weekly_mission_1/example2/main.js
+++ playbook-main/weekly_mission_1/example2/main.js
@@ -1,6 +1,9 @@
 // node main.js
 
+//Se crea objeto logger el cual contiene las propiedades establecidas en el archivo logger.js
 const logger = require('./logger')
 
+
+// se accede a las funciones de logger
 logger.info('This is an informational message')
 logger.verbose('This is a verbose message')
